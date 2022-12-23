9c44d7bf73bc5d259635a12c17ec034693f4e146
gogy9000 <goropashnyj9000@gmail.com>
2022-12-23 16:35:41 +0500

    install vectorIcons

--- littleAinstein/app/components/ui/button/Button.tsx
+++ littleAinstein/app/components/ui/button/Button.tsx
@@ -1,6 +1,6 @@
 import React, {memo, PropsWithChildren} from 'react'
 import {ActivityIndicator, Pressable, PressableProps, Text, TextStyle, View} from 'react-native'
-import {Loader} from "../loader/Loader";
+import Icon from 'react-native-vector-icons/Ionicons';
 
 interface IButtonProps extends PressableProps {
     title?: string
@@ -13,7 +13,9 @@ export const Button: React.FC<PropsWithChildren<IButtonProps>> = memo(({children
     const childrenIsString=typeof children === 'string'
     return (
         <Pressable className={'flex-row space-x-1 justify-center bg-amber-500 rounded-xl py-2 px-3  items-center'} {...rest}>
-            {isLoading?<ActivityIndicator color={'rgb(255,255,255)'}/>:null}
+            {isLoading?<ActivityIndicator color={'rgb(255,255,255)'}/>:
+                <Icon name="ios-person" size={24} color={"rgb(255,255,255)"} />
+            }
             {title ? <Text className={'text-xl font-semibold text-white'}{...textStyle}>{title}</Text> : null}
             {childrenIsString?<Text className={'text-xl font-semibold text-white'}{...textStyle}>{children}</Text>:children}
         </Pressable>
